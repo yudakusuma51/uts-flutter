ee2ef8e5f146039da140225bb084a9bddd56796e
Yuda Kusuma <yudatugas123@gmail.com>
2026-04-26 22:54:26 +0800

    create dashboard page

--- lib/dashboard-screen.dart
+++ lib/dashboard-screen.dart
@@ -58,7 +58,7 @@ class DashboardScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard User"),
+        title: Text("Dashboard"),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
